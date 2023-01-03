b3288e8c89266fe6086f97ac70a9ddf80c5fa645
Stephen Kelly <stephen.kelly.94@hotmail.com>
2023-01-03 11:40:54 +0100

    Update setup for pacman

--- .config/my-setup.sh
+++ .config/my-setup.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 set -e -o pipefail # fail on error and report it, debug all lines
 
-echo "Adding and updating repos"
-sudo add-apt-repository ppa:aslatter/ppa -y
-sudo add-apt-repository ppa:neovim-ppa/unstable -y
-sudo apt-get update
-
-install_packages="zip unzip git curl alacritty neovim tmux zsh"
+install_packages="zip unzip git curl alacritty neovim tmux zsh ripgrep fd"
 
 # Detect package manager
-echo "Installing the must-have pre-requisites"
 if [[ $(command -v apt-get) ]]; then
     echo "Detected apt-get"
+    echo "Adding and updating repos"
+    sudo add-apt-repository ppa:aslatter/ppa -y
+    sudo add-apt-repository ppa:neovim-ppa/unstable -y
+    sudo apt-get update
+
+    echo "Installing the must-have pre-requisites"
     sudo apt-get install -y $install_packages
 elif [[ $(command -v pacman) ]]; then
-    sudo pacman -S $install_packages
+    echo "Installing the must-have pre-requisites"
+    sudo pacman -Syu $install_packages
 fi
+
+echo "Installing NVM, remember to install node"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "Installing oh my zsh and plugins"
 if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
