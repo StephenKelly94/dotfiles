62ac82fc309db2e7dd1e239f8136f4fe79583722
Stephen Kelly <stephen.kelly.94@hotmail.com>
2023-11-19 00:24:26 +0100

    Add onecommand

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,6 +1,42 @@
 return {
     {
-        dir = "/home/sdk/dev/OneCommand.nvim/",
+        "stephenkelly94/onecommand.nvim",
+        keys = {
+            {
+                "<leader>xc",
+                function ()
+                    require('onecommand').prompt_run_command()
+                end,
+                desc = "Run command",
+            },
+            {
+                "<leader>xh",
+                function ()
+                    require('onecommand').view_history()
+                end,
+                desc = "View command history",
+            },
+            {
+                "<leader>xl",
+                function ()
+                    require('onecommand').show_last_command_output()
+                end,
+                desc = "Show last command output",
+            },
+        },
+        -- opts = {
+        --     ui = {
+        --         line_numbers = false,
+        --         relative_numbers = false,
+        --         modifiable = false,
+        --         popup = {
+        --             footer ="TEST"
+        --         },
+        --         command = {
+        --             command_limit = 1
+        --         }
+        --     }
+        -- }
     },
     {
         "tpope/vim-fugitive",
