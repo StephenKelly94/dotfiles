42564983855c59ff07cccbfd541f494d4f174145
Stephen Kelly <stephen.kelly.94@hotmail.com>
2025-09-13 17:04:23 +0200

    Set tabstap to 2

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -10,8 +10,8 @@ opt.relativenumber = true
 opt.number = true
 
 -- Tabs & Indentation
-opt.tabstop = 4
-opt.shiftwidth = 4
+opt.tabstop = 2
+opt.shiftwidth = 2
 opt.expandtab = true
 opt.autoindent = true
 
